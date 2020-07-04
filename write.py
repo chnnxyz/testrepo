090609369bb8d84d1b46c75e259664e7dcd91420
chnnxyz <chnnxyz@github.com>
2020-07-04 13:06:58 -0500

    testing

---
+++ write.py
@@ -0,0 +1,10 @@
+def percentage(w,row):
+    if w='wins':
+        return row[1]/(row[1]+row[2]+row[3])
+    elif w = 'losses':
+        pass
+
+with open(csvfile,'r') as csvdude:
+    creader=csv.reader(csvdude)
+    for row in creader:
+        print(percentage('wins',row))
